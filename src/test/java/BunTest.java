207f3d24c1665a81e87188df982744ce8d4dec8d
rzhao <dns.mail@mail.ru>
2025-05-10 10:31:39 +0700

    Added BunTest.java

---
+++ src/test/java/BunTest.java
@@ -0,0 +1,32 @@
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import praktikum.Bun;
+
+import static org.junit.Assert.assertEquals;
+
+//@RunWith(MockitoJUnitRunner.class)
+public class BunTest {
+    @Mock
+    private static String name;
+    @Mock
+    private static float price;
+    private static Bun bun;
+
+    @BeforeClass
+    public static void init() {
+        bun = new Bun(name, price);
+    }
+
+    @Test
+    public void getNameMockNameAssertValid() {
+        assertEquals(name, bun.getName());
+    }
+
+    @Test
+    public void getPriceMockPriceAssertValid() {
+        assertEquals(price, bun.getPrice(), 0.0001);
+    }
+}
